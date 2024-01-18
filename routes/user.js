53175b34e12edcb889eda8038f88f3d99537ca4b
ElianJ09 <erodriguez116@alumnos.uaq.mx>
2024-01-18 15:22:22 -0600

    Added a Login with JWT

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -17,6 +18,28 @@ user.post("/", async (req, res, next) => {
             res.status(500).json({code: 500, message: "Ocurrio un problema al agregar el usuario!"})
     }
     return res.status(500).json({code:500, message: "Campos imcompletos, llenalos correctamente!"})
+});
+
+user.post("/login", async (req, res, next) =>{
+    const {user_mail, user_password} = req.body;
+
+    if(user_mail && user_password){
+        const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`;
+        const ExistRow = await db.query(query);
+
+        if(ExistRow.length == 1){
+            const token = jwt.sign({
+                user_id: ExistRow[0].user_id,
+                user_name: ExistRow[0].user_name
+            }, "debugkey");
+    
+            return res.status(200).json({code: 200, message: "Autenticacion correcta!, enviando token...", token: token});
+        }
+        else{
+            return res.status(401).json({code: 401, message: "Los datos son incorrectos!"})
+        }
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos, llenelos correctamente!"})
 });
 
 user.get("/", async (req, res, next) =>{
