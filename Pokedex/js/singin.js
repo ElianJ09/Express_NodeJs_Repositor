7b9fec4b30c6f8665eb27b1ae576701c1fb08422
ElianJ09 <erodriguez116@alumnos.uaq.mx>
2024-01-21 21:11:42 -0600

    Added the signin functionaly for the UI

--- Pokedex/js/singin.js
+++ Pokedex/js/singin.js
@@ -2,25 +2,28 @@ window.onload = init;
 
 function init() {
     document.querySelector('.btn-secondary').addEventListener('click', function(){
-        window.location.href = "signin.html"
+        window.location.href = "login.html"
     });
 
-    document.querySelector('.btn-primary').addEventListener('click', login);
+    document.querySelector('.btn-primary').addEventListener('click', signin);
 }
 
-function login(){
+function signin(){
     var mail = document.getElementById('input-mail').value;
+    var name_user = document.getElementById('input-name').value;
     var password = document.getElementById('input-password').value; 
 
     axios({
         method: 'post',
-        url: 'http://localhost:3000/user/login',
+        url: 'http://localhost:3000/user/signin',
         data: {
+            user_name: name_user,
             user_mail: mail,
             user_password: password
         }
     }).then(function(res) {
-        console.log(res);
+        alert("Registro del usuario exitoso!");
+        window.location.href = "login.html";
     }).catch(function(err){
         console.log(err);
     });
